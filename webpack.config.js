fe10141091dd7d1572942caabbfb75ac7ff8e345
Joe Lopez <lopez.4.joseph@gmail.com>
2021-02-06 20:56:53 -0700

    Change webpack route

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const loader = require('sass-loader');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const loader = require("sass-loader");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
   },
-  devtool: 'inline-source-map',
-  mode: 'development',
+  devtool: "inline-source-map",
+  mode: "development",
   // CSS and file (image) loaders
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-              // {
-              //   loader: resolve-url-loader,
-              //   options: {
-              //     sourceMap: true
-              //   }
-              // },
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'images/',
-                  publicPath: 'images/'
-                }
-              }
-            ]
-      }
+          // {
+          //   loader: resolve-url-loader,
+          //   options: {
+          //     sourceMap: true
+          //   }
+          // },
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/",
+            },
+          },
+        ],
+      },
     ],
   },
   // Below is needed for webpack-dev-server
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
+      template: "./docs/index.html",
+    }),
   ],
   devServer: {
-         contentBase: './dist'
-  }
+    contentBase: "./dist",
+  },
 };
